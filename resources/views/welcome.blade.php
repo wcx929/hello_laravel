28c13205919112e1c4bc275d79f00bc6d1d39bc8
Wcx <1161993909@qq.com>
2020-02-10 12:07:03 +0000

    Say something

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Windy</div>
+                <div class="title">Hello Laravel! - by Windy111</div>
             </div>
         </div>
     </body>
